a30cc8eacf60a7425a88a1621ae10f279e36c43c
unknown <vitor.sofiati@live.com>
2024-04-22 15:05:23 -0300

    Criação do módulo de fetch na API

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,11 @@
-"use strict";
-console.log('Inicial');
+import fetchData from './fetchData.js';
+async function handleData() {
+    const data = await fetchData('https://api.origamid.dev/json/transacoes.json');
+    if (data) {
+        data.forEach((item) => {
+            console.log(item['Cliente Novo']);
+        });
+    }
+}
+handleData();
 //# sourceMappingURL=main.js.map
