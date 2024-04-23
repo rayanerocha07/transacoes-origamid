a64a5a7e1a8346d06f7f91d3b545bec71e4e1709
unknown <vitor.sofiati@live.com>
2024-04-23 18:04:18 -0300

    Final

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,65 @@
+import Estatisticas from './Estatisticas.js';
+import { CountList } from './countBy.js';
 import fetchData from './fetchData.js';
-
-type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito';
-type TransacaoStatus =
-  | 'Paga'
-  | 'Recusada pela operadora de cartão'
-  | 'Aguardando pagamento'
-  | 'Estornada';
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: string;
-  Email: string;
-  ['Valor (R$)']: string;
-  ['Forma de Pagamento']: TransacaoPagamento;
-  ['Cliente Novo']: number;
-}
+import normalizarDados from './normalizarDados.js';
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
-    'https://api.origamid.dev/json/transacoes.json'
+    'https://api.origamid.dev/json/transacoes.json?'
   );
 
-  if (data) {
-    data.forEach((item) => {
-      console.log(item['Cliente Novo']);
+  // Trava a função caso não exista data
+  if (!data) return;
+
+  const transacoes = data.map(normalizarDados);
+  preencherTabela(transacoes);
+  preencherEstatisticas(transacoes);
+}
+
+function preencherLista(lista: CountList, containerId: string): void {
+  const containerElement = document.getElementById(containerId);
+  if (containerElement) {
+    Object.keys(lista).forEach((key) => {
+      containerElement.innerHTML += `<p>${key}: ${lista[key]}</p>`;
     });
   }
+}
+
+function preencherEstatisticas(transacoes: Transacao[]): void {
+  const data = new Estatisticas(transacoes);
+  console.log(data);
+
+  preencherLista(data.pagamento, 'pagamento');
+  preencherLista(data.status, 'status');
+
+  const totalElement = document.querySelector<HTMLElement>('#total span');
+  if (totalElement) {
+    totalElement.innerText = data.total.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  }
+
+  const diaElement = document.querySelector<HTMLElement>('#dia span');
+  if (diaElement) {
+    diaElement.innerText = data.melhorDia[0];
+  }
+}
+
+function preencherTabela(transacoes: Transacao[]): void {
+  const tabela = document.querySelector('#transacoes tbody');
+  if (!tabela) return;
+  transacoes.forEach((transacao) => {
+    tabela.innerHTML += `
+      <tr>
+        <td>${transacao.nome}</td>
+        <td>${transacao.email}</td>
+        <td>R$ ${transacao.moeda}</td>
+        <td>${transacao.pagamento}</td>
+        <td>${transacao.status}</td>
+      </tr>
+    `;
+  });
 }
 
 handleData();
